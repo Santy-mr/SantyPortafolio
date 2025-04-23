f50cc095e6a6a63651e1e07d1ce97dee1709a7d1
Santiago Mondragon <sty.mondragon@gmail.com>
2025-04-22 18:56:04 -0600

    Update Vercel Deploy

--- src/js/LoadingScreen.js
+++ src/js/LoadingScreen.js
@@ -1,7 +1,6 @@
 import Application from "./Application.js";
 import EventEmitter from "../utils/EventEmitter.js";
 
-
 export default class LoadingScreen extends EventEmitter {
     constructor(){
         super()
